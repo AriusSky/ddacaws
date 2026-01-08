efdcc964e29b6fbbb3baeb3be4ee86871a8e1ff1
AriusSky <eternalzsky@gmail.com>
2026-01-08 16:29:15 +0800

    port number

--- backend/src/HealthcareAPI/Program.cs
+++ backend/src/HealthcareAPI/Program.cs
@@ -136,7 +136,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+        policy.WithOrigins("http://localhost:80")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
